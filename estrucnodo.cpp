117f19231a5f3d566dea6314ba4b9c5b4ec7ae65
uziel01 <uzfl27@gmail.com>
2019-10-10 10:06:39 -0500

    Es un programa que permite a base de nodod y punteros ingresar datos enteros y posteriror mente mostrarlos ordenados en pantalla a travez de un menu

--- estrucnodo.cpp
+++ estrucnodo.cpp
@@ -1,71 +1,77 @@
-#include <iostream>
-#include<conio.h>
-using namespace std; 
+#include <bits/stdc++.h>
+using namespace std;
 
 struct nodo{
-	int valor;
-	struct nodo *Ptr;
-	
-}; 
+    int entero;
+    nodo *nodo2;
+    nodo(int newvalor){
+    	entero=newvalor;
+    	nodo2=NULL;
+	}
+	void ingresar(int newvalor){
+    nodo *po=nodo2;
+    nodo *newnodo=new nodo(newvalor);
+    if(po!=NULL) while(po->nodo2!=NULL) po=po->nodo2;
+    if(po==NULL) nodo2=newnodo;
+    else po->nodo2=newnodo;
+    }
+    void ordenar(int newvalor){
+	nodo *po=nodo2;
+    nodo *newnodo=new nodo(newvalor);
+    nodo *before=this;
+	if(po==NULL){
+	before->nodo2=newnodo;
+    return;
+    }else while(po!=NULL && newvalor>po->entero){
+        before=po;
+        po=po->nodo2;
+    }
+    newnodo->nodo2=before->nodo2;
+	before->nodo2=newnodo;
+    }
+    void mostrar(){
+    nodo *po=nodo2;
+    while(po!=NULL){
+    cout<<po->entero<<endl;
+    po=po->nodo2;
+        }
+    }
+};
+nodo l1(3);
 
-
-void agregar (int, nodo *&);
-void mostrarDatos(nodo *&);
-
-int main(int argc, char** argv){
-	int valor;
-	struct  nodo *inicio;
-	struct  nodo *aux;
-	int n;
-	inicio=NULL;
-	
+int main()
+{
+	int opcion;
 	do{
-	
-	
-	cout<<"\n Ingresa una opcion sobre los datros: \n";
-	cout<<"1: Leer datos : \n";
-	cout<<"2: Mostrar datos : \n";
-	
-	cin >> n;
-        switch(n)
-        {
-            case 1:
-            	int val;
-            	while(val!=NULL){
-            	cout<<"Ingresa un valor: ";
-            	cin>>val;
-				agregar(val, inicio);
-			}
-            break;
-            case 2: 
-			mostrarDatos(inicio);
-            break;
-            default: 
-            cout << "Opcion desconocida!" << endl;
-            break;
-		}    
-    } while (n != 3); 
-	getch();
-	return 0;
-}
+		system("cls");
+		cout<<".::Elija una opcion::."<<endl;
+		cout<<"1. Ingresar datos"<<endl;
+		cout<<"2. Mostrar datos ordenados"<<endl;
+		cout<<"3. Salir"<<endl;
+		cout<<"Opcion: ";
+		cin>>opcion;
+		switch(opcion){
 
-void agregar (int valor, nodo *&Ptr){	
-	struct  nodo *inicio2;
-	struct  nodo *aux2;
-	
-		inicio2=Ptr;
-		aux2= new nodo();
-		aux2->valor=valor;
-		aux2->Ptr=NULL;
-	if (inicio2==NULL){
-		Ptr=aux2;
-	}
-}
+		case 1:	
+		int newvalor;
+		cout<<"Ingresar datos\n";
+		cin>>newvalor;
+		l1.ordenar(newvalor);
+		break;
+		case 2:
+		cout<<"Los valores son: "<<endl;
+		l1.mostrar();
+		system("pause");	
+		break;
+		case 3:
+		cout<<"Vuelva pronto"<<endl;
+		break;	
+		default:
+		cout<<"Ingrese una opcion valida "<<endl;
+		system("pause");
+		break;
+	    }
+	}while(opcion!=3);
 
-void mostrarDatos(nodo *&Ptr){
-	while(Ptr!=NULL){
-		cout<<"El valor es: " <<Ptr->valor;
-		Ptr=Ptr->Ptr;
-		
-	}
+	 return 0;
 }
